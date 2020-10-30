57468764ccccf87abebf2604863b9df232d3eed7
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-10-30 16:30:16 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.timerId = null;
+    this.tick = this.tick.bind(this);
+    this.state = { currentTime: new Date().toLocaleTimeString() };
+  }
+  tick() {
+    this.setState({ currentTime: new Date().toLocaleTimeString() });
+  }
+  componentDidMount() {
+    this.timerId = setInterval(this.tick, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.currentTime}</h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
